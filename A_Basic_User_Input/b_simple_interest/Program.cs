4b2ba145f162b174322055b4367935aed8f80f9e
Mina Karki <minakarki012@gmail.com>
2026-06-05 22:57:38 +0545

    Basic User Input

---
+++ A_Basic_User_Input/b_simple_interest/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace b_simple_interest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the value of P:");
+        double p = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter the value of T:");
+        double t = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter the value of R:");
+        double r = Convert.ToDouble(Console.ReadLine());
+
+        double si = (p * t * r) / 100;
+
+        Console.WriteLine("Simple Interest = " + si);
+    }
+}
+
