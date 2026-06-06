3458ecb1b0b037352c28bf5283c284245142aa84
Mina Karki <minakarki012@gmail.com>
2026-06-06 17:06:33 +0545

    Class and Object

---
+++ B_Class_Object/c_class_and_object_user_input/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace c_class_and_object_user_input;
+#nullable disable
+class Addition
+{
+    int a;
+    int b;
+    static void Main(string[] args)
+    {
+        Addition a1 = new Addition();
+        Console.WriteLine("Enter the value of a:");
+        a1.a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the value of b:");
+        a1.b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("The sum of a and b is : " +(a1.a+a1.b));
+    }
+}
