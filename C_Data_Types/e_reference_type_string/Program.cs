b5d4a39ead551d453a32016ab983b8c3d2a29b1e
Mina Karki <minakarki012@gmail.com>
2026-06-17 14:28:04 +0545

    Reference Type String

---
+++ C_Data_Types/e_reference_type_string/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace e_reference_type_string;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "Ram";
+        name += " Bahadur";
+        Console.WriteLine(name);
+        name = name + " Thapa";
+        Console.WriteLine(name);
+    }
+}
