33287256a5ed01bc9790296471f4ddd003694466
Mina Karki <minakarki012@gmail.com>
2026-06-17 12:59:51 +0545

    Data Types

--- C_Data_Types/a_value_type/obj/Debug/net10.0/a_value_type.AssemblyInfo.cs
+++ C_Data_Types/a_value_type/obj/Debug/net10.0/a_value_type.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("a_value_type")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3458ecb1b0b037352c28bf5283c284245142aa84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c577c8705c70c4011e782398d78967ca51ca13e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("a_value_type")]
 [assembly: System.Reflection.AssemblyTitleAttribute("a_value_type")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ C_Data_Types/c_value_type_byte/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace c_value_type_byte;
+class Program
+{
+    static void Main(string[] args)
+    {
+        byte number = 10;
+
+        // byte is an 8-bit unsigned integer
+        Console.WriteLine("Initial value: " + number);
+
+        number++;
+        Console.WriteLine("After increment: " + number);
+
+        number = 255;
+
+        Console.WriteLine("Maximum byte value: " + number);
+
+        number++;   // Overflow occurs here
+        Console.WriteLine("After overflow: " + number);
+
+        number++;
+        Console.WriteLine("Next value: " + number);
+    }
+}
