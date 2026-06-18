7afadb0760cf75ed2723fcefada0f57cc53777f9
Mina Karki <minakarki012@gmail.com>
2026-06-18 12:04:43 +0545

    If Else Statement

---
+++ D_Control_Statements/b_if_else_odd_even/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace b_if_else_odd_even;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Enter a number to check whether the nummber is odd or even : ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("The number is even." + number);
+        }
+        else
+        {
+            Console.WriteLine("The number is odd." + number);
+        }
+    }
+}
