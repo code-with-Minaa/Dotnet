e058f1c31e94158a00d3faf62d8804aba9ceaeca
Mina Karki <minakarki012@gmail.com>
2026-06-25 22:14:30 +0545

    While Loop

---
+++ D_Control_Statements/C_Looping_Statements/b_while_odd/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace b_while_odd;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 19;
+
+        while (a >= 1)
+        {
+            Console.WriteLine(a);
+            a -= 2;
+        }
+    }
+}
