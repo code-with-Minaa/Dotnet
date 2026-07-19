c7f9c325063bbcebff2a297035dca5f3704fa97c
Mina Karki <minakarki012@gmail.com>
2026-07-19 13:07:05 +0545

    Class and Object

---
+++ B_Class_Object/a_class_and_object/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace a_class_and_object;
+#nullable disable
+class Employee
+{
+    int eid; 
+    public void input()
+    {
+        eid = 10;
+    }
+    public void display()
+    {
+        Console.WriteLine("Employee ID : " + eid);
+    }
+
+    static void Main(string[] args)
+    {
+        Employee e1 = new Employee();
+        e1.input();
+        e1.display();
+    }
+}
