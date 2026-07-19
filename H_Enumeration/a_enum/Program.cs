37da60dc782517209e0211596bc4313009d78244
Mina Karki <minakarki012@gmail.com>
2026-07-19 13:14:46 +0545

    Enum

---
+++ H_Enumeration/a_enum/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace a_enum;
+
+class Program
+{
+    enum Months
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+
+    static void Main(string[] args)
+    {
+        // Convert enum to integer
+        int monthNumber = (int)Months.December;
+        Console.WriteLine(monthNumber);
+
+        // Convert integer to enum
+        var month = (Months)4;
+        Console.WriteLine(month);
+    }
+}
---
+++ H_Enumeration/b_enum/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace b_enum;
+
+class Program
+{
+    enum TrafficLight { Red, Yellow, Green };
+
+    static void Main(string[] args)
+    {
+        var signal = TrafficLight.Red;
+
+        if (signal == TrafficLight.Red)
+        {
+            Console.WriteLine("Stop");
+        }
+        else if (signal == TrafficLight.Yellow)
+        {
+            Console.WriteLine("Get Ready");
+        }
+        else
+        {
+            Console.WriteLine("Go");
+        }
+    }
+}
