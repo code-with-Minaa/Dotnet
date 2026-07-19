0bedae2a6376ef8ae482e3415850de144b5c807a
Mina Karki <minakarki012@gmail.com>
2026-07-19 13:05:13 +0545

    Basic User Input

---
+++ A_Basic_User_Input/c_perimeter_of_rectangle/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace c_perimeter_of_rectangle;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter Length : ");
+        int l = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter Breadth : ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        int perimeter = 2 * (l + b);
+
+        Console.WriteLine("The perimeter of a rectangle is : " + perimeter);
+    }
+}
---
+++ A_Basic_User_Input/d_you_can_vote_or_not/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace d_you_can_vote_or_not;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your age : ");
+        int age = Convert.ToInt32(Console.ReadLine());
+        if (age > 18)
+        {
+            Console.WriteLine("You can vote " + age);
+        }
+        else
+        {
+            Console.WriteLine("You can not vote " + age);
+        }
+    }
+}
+
