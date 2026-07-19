915d945879f5714dbb76566daab8674e5786a5d5
Mina Karki <minakarki012@gmail.com>
2026-07-19 13:18:58 +0545

    Sealed Keyword

---
+++ L_Sealed/a_sealed/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace a_sealed;
+
+//sealed class SuperClass
+class SuperClass
+{
+    public void hi()
+    {
+        Console.WriteLine("Hii from SuperClass.");
+    }
+}
+
+class SubClass : SuperClass
+{
+    public void hello()
+    {
+        Console.WriteLine("Hello from SubClass.");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        SubClass s1 = new SubClass();
+        s1.hi();
+        s1.hello();
+    }
+}
