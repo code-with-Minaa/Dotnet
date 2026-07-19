e1d4c680cbc83bf0e3788b48b53c7d1a109faa36
Mina Karki <minakarki012@gmail.com>
2026-07-19 13:16:22 +0545

    Partial Class

---
+++ I_Partial_Class/a_partial_class/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace a_partial_class;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s1 = new Student();
+
+        s1.Hi();
+
+    }
+}
---
+++ I_Partial_Class/a_partial_class/Student.cs
@@ -0,0 +1,9 @@
+namespace a_partial_class;
+
+partial class Student
+{
+    public void Hi()
+    {
+        Console.WriteLine("Hi, I am your student.");
+    }
+}
