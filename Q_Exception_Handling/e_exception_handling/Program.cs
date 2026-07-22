57c57ff208d2074a4d113389df76de17fae41a09
Mina Karki <minakarki012@gmail.com>
2026-07-22 21:46:41 +0545

    Exception Handling

---
+++ Q_Exception_Handling/e_exception_handling/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace e_exception_handling;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // string value = null;
+        string value = "Shyam";
+        try
+        {
+            // if(value.Length == 0){
+            if(value.Length != 0){
+                Console.WriteLine(value);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception found " + e);
+        }
+    }
+}
