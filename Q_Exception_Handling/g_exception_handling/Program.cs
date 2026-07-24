a0363ecc7bbda52c1e59d13746f682a5f69a199c
Mina Karki <minakarki012@gmail.com>
2026-07-24 06:35:27 +0545

    Exception Handling

---
+++ Q_Exception_Handling/g_exception_handling/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace g_exception_handling;
+
+class MyException : Exception
+{
+    public MyException(string str)
+    {
+        Console.WriteLine(str);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyException obj = new MyException("This is user defined exception.");
+        try
+        {
+            throw obj;
+        }catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
---
+++ Q_Exception_Handling/h_exception_handling/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace h_exception_handling;
+
+class InvalidVoter : Exception
+{
+    public InvalidVoter(string message) : base (message) {}
+}
+class Program
+{
+    static void AgeValidator(int age)
+    {
+        if (age < 18)
+        {
+            throw new InvalidVoter ("You are not eligible to get your voter ID by age.");
+        }
+        else
+        {
+            Console.WriteLine("You are eligible to get voter ID by your age.");
+        }
+    }
+    static void Main(string[] args)
+    {
+       Console.WriteLine("Enter your age : ");
+       int a = Convert.ToInt32(Console.ReadLine());
+        try
+        {
+            AgeValidator(a);
+        }catch (InvalidVoter e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
+
