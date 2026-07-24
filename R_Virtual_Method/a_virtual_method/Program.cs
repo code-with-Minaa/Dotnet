49f6519325eb4d66c2bdbb97cea53c7058bca10c
Mina Karki <minakarki012@gmail.com>
2026-07-24 08:31:38 +0545

    Virtual Method

---
+++ R_Virtual_Method/a_virtual_method/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace a_virtual_keyword;
+
+class SuperClass
+{
+    //virtual method
+    public virtual int  Calc(int a, int b)
+    {
+        return (a + b);
+    }
+}
+
+class SubClass : SuperClass
+{
+    //redefined
+    public override int  Calc(int a, int b)
+    {
+        return (a + b + 5);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        SubClass obj = new SubClass();
+        Console.WriteLine("Redefined result = " + obj.Calc(10,50));
+    }
+}
---
+++ R_Virtual_Method/b_virtual_method/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace b_virtual_method;
+
+class Shape
+{
+    protected int width, height;
+
+    public Shape(int a = 0, int b = 0)
+    {
+        width = a;
+        height = b;
+    }
+
+    // Virtual Method
+    public virtual int area()
+    {
+        return 0;
+    }
+}
+
+// Derived Class Rectangle
+class Rectangle : Shape
+{
+    public Rectangle(int a = 0, int b = 0) : base(a, b){}
+
+    public override int area()
+    {
+        Console.WriteLine("Rectangle Area:");
+        return width * height;
+    }
+}
+
+// Derived Class Triangle
+class Triangle : Shape
+{
+    public Triangle(int a = 0, int b = 0) : base(a, b) {}
+
+    public override int area()
+    {
+        Console.WriteLine("Triangle Area:");
+        return (width * height) / 2;
+    }
+}
+
+class Caller
+{
+    public void CallArea(Shape sh)
+    {
+        int ar;
+        ar = sh.area();
+        Console.WriteLine("Area = {0}", ar);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Caller obj = new Caller();
+        Rectangle r = new Rectangle(10,12);
+        Triangle t = new Triangle(55,12);
+        obj.CallArea(r);
+        obj.CallArea(t);
+    }
+}
