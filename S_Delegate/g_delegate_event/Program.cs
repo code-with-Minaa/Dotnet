a79ea3fcf60553434d386e95dc93abcb190faabb
Mina Karki <minakarki012@gmail.com>
2026-08-02 18:38:08 +0545

    Delegate

---
+++ S_Delegate/g_delegate_event/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace g_delegate;
+
+public delegate string MyDel (string str);
+class EventPro
+{
+    event MyDel MyEvent;
+
+    public EventPro()
+    {
+        this.MyEvent += new MyDel (this.DisplayMessage);
+    }
+
+    public string DisplayMessage(string username)
+    {
+        return "Hello " + username;
+    }
+    static void Main(string[] args)
+    {
+        EventPro obj = new EventPro();
+        string result = obj.MyEvent("Sanothimi Campus");
+        Console.WriteLine(result);
+    }
+}
