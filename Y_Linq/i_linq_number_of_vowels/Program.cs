aba46cf9667866defdba282f737d65c578a02db6
Mina Karki <minakarki012@gmail.com>
2026-08-22 21:13:46 +0545

    Linq

---
+++ Y_Linq/i_linq_number_of_vowels/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace i_linq_number_of_vowels;
+#nullable disable
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a sentence :"); 
+        var sentence = Console.ReadLine();
+
+        var vowels = new char [] {'A', 'E', 'I', 'O', 'U'};
+        var query = from letter in sentence.ToUpper() where vowels.Contains(letter) select letter;
+        var count = query.Count();
+        Console.WriteLine("The number of vowels = {0}", count);
+    }
+}
